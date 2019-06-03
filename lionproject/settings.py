047b2dfea8f0207f1df3b099c961d24259c3e937
Yangsu0 <yjy0186@naver.com>
2019-06-03 22:12:31 +0900

    herokuu

--- lionproject/settings.py
+++ lionproject/settings.py
@@ -20,11 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c_z=@i$t1-l_owks*z_%orhzbx+z0ix^=@2ccjp19u!h-1u_d)'
-
+#SECRET_KEY = 'c_z=@i$t1-l_owks*z_%orhzbx+z0ix^=@2ccjp19u!h-1u_d)'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 ALLOWED_HOSTS = ['*']
 
 
